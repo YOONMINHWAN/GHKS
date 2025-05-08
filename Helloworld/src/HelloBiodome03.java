2ce309f82ad69ac9bc4ce97262f68f733a6a6f2c
yoonminhwan <ghks681543@gmail.com>
2025-05-08 22:16:30 +0900

    YOONMINHWAN

---
+++ Helloworld/src/HelloBiodome03.java
@@ -0,0 +1,5 @@
+public class HelloBiodome03 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Helloworld/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] fruits = {"Apple", "Banana", "Orange", "Strawberry", "Mango", "Pineapple"};
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+    }
+
+}
