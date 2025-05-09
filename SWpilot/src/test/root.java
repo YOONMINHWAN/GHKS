9b9a3415be35044a916d4baece448d0618a66e75
yoonminhwan <ghks681543@gmail.com>
2025-05-09 17:34:54 +0900

    YOONMINHWAN

--- SWpilot/src/test/root.java
+++ SWpilot/src/test/root.java
@@ -1,17 +1,9 @@
 package test;
 
 public class root {
-    public static double roots(double a) {
-        double guess = a / 2;
-        double epsilon = 0.0000000001;
-
-        while (abs(guess*guess - a) < epsilon) {
-            guess = (guess + a / guess) / 2;
-        }
-        return guess;
-    }
-    public static double abs(double a) {
-        return a < 0 ? -a : a;
+    public static void main(String[] args) {
+        double a1 = 60;
+        System.out.println(Math.sqrt(a1));
     }
 }
 
