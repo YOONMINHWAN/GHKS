74c9d30b7dbeeed103c4bf9606e15e05e06c9d01
yoonminhwan <ghks681543@gmail.com>
2025-05-09 17:23:06 +0900

    YOONMINHWAN

--- SWpilot/src/HelloBiodome/HelloBiodome03.java
+++ SWpilot/src/HelloBiodome/HelloBiodome03.java
@@ -8,6 +8,9 @@ public class HelloBiodome03 {
         humidity = Double.parseDouble(args[1]);
         oxygen = Double.parseDouble(args[2]);
 
+        System.out.println("온도 : " + temperature);
+        System.out.println("습도 : " + humidity);
+        System.out.println("산소 농도 : " + oxygen);
         System.out.println("생명지수 H = " + calculate(humidity, temperature, oxygen));
         }
         catch (NumberFormatException e) {
@@ -15,20 +18,21 @@ public class HelloBiodome03 {
         }
     }
     public static final double PI = 3.14;
+    public static final double UB = 0.415;
     public static double root(double a) {
         double guess = a / 2;
         double epsilon = 0.0000000001;
 
-        while (abs1(guess*guess - a) < epsilon) {
+        while (abs(guess*guess - a) > epsilon) {
             guess = (guess + a / guess) / 2;
         }
         return guess;
     }
-    public static double abs1(double a) {
+    public static double abs(double a) {
         return a < 0 ? -a : a;
     }
-    public static double calculate(double e, double f, double g) {
-        double result = root(e) - f;
-        return abs1(result) + g/PI;
+    public static double calculate(double a, double f, double g) {
+        double result = root(a) - f;
+        return UB * abs(result) + g / (PI*PI);
     }
 }
--- SWpilot/src/test/root.java
+++ SWpilot/src/test/root.java
@@ -1,25 +1,17 @@
 package test;
 
 public class root {
-    public static double sqrt(double n) {
-        if (n < 0) throw new IllegalArgumentException("음수는 불가");
+    public static double roots(double a) {
+        double guess = a / 2;
+        double epsilon = 0.0000000001;
 
-        double guess = n / 2.0;
-        double epsilon = 1e-10;
-
-        while (abs(guess * guess - n) > epsilon) {
-            guess = (guess + n / guess) / 2.0;
+        while (abs(guess*guess - a) < epsilon) {
+            guess = (guess + a / guess) / 2;
         }
-
         return guess;
     }
-
-    private static double abs(double value) {
-        return (value < 0) ? -value : value;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(sqrt(3));    // 대략 1.414
+    public static double abs(double a) {
+        return a < 0 ? -a : a;
     }
 }
 
