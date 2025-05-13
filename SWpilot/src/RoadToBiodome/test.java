e7d3dfc4ae3a38ac6a29dd1cb993d78b771ca94e
yoonminhwan <ghks681543@gmail.com>
2025-05-13 10:32:54 +0900

    YOONMINHWAN

--- SWpilot/src/RoadToBiodome/test.java
+++ SWpilot/src/RoadToBiodome/test.java
@@ -1,36 +1,38 @@
 package RoadToBiodome;
 
 import java.util.ArrayList;
-import java.util.List;
 
-public class RoadToBiodome01 {
+public class test {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
         int lo = args.length;
+
         for (int i = 0; i < args.length; i++) {
-            if (Integer.parseInt(args[i]) >= 0 && Integer.parseInt(args[i]) <= 1000) {
-                list.add(Integer.parseInt(args[i]));
+            int num = Integer.parseInt(args[i]);
+            if (num >= 0 && num <= 1000) {
+                list.add(num);
             } else {
                 System.out.println("입력된 값의 범위가 올바르지 않습니다. 0에서 1000까지의 값을 입력해주세요!");
                 return;
             }
         }
+
         int result = sub(lo, list);
         System.out.println(result);
     }
+
     public static int sub(int lo, ArrayList<Integer> list) {
         for (int i = 0; i < lo; i++) {
             int count = 0;
             for (int j = 0; j < lo; j++) {
-                if (i != j && list.get(i).equals((list.get(j)))) {
+                if (i != j && list.get(i).equals(list.get(j))) {
                     count++;
                 }
             }
-            if (count == 0) {
+            if (count == 0) {  // 오직 한 번만 나온 값
                 return list.get(i);
             }
         }
-        return -1;
+        return -1; // 중복되지 않은 값이 없을 경우
     }
-
 }
