4405eb8b0dde83cdf2d01caeb0be88d849b10d16
yoonminhwan <ghks681543@gmail.com>
2025-06-01 19:20:34 +0900

    YOONMINHWAN

--- SWpilot/src/BiodomeFamily/BiodomeFamily01.java
+++ SWpilot/src/BiodomeFamily/BiodomeFamily01.java
@@ -6,26 +6,37 @@ import java.util.Scanner;
 
 class Organism {
     static String name = new String();
-    static String species = new String();
-    static String habitat = new String();
+    String species = new String();
+    String habitat = new String();
     public static void displayInfo() {
 
     }
 }
 
 class LifeNest {
-    static List<LifeNest> organismlist = new ArrayList<>();
+    static List<String> organismlist = new ArrayList<>();
 
     public static void management() {
-        organismlist.get(Integer.parseInt(Organism.name));
-        System.out.println("[LifeNest]" + Organism.name + "이(가) 추가되었습니다.");
+        for (int i = 0; i < organismlist.size(); i+=3) {
+            System.out.println("[LifeNest]" + organismlist.get(i) + "이(가) 추가되었습니다.");
+        }
     }
 }
 
 public class BiodomeFamily01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Organism.name = scanner.nextLine();
-        LifeNest.management();
+        while (true) {
+            System.out.println("동식물의 이름과 종, 서식지를 입력해 주십시오. [ex) 펭귄, 동물, 남극]");
+            Organism.name = scanner.nextLine();
+            for (int i = 0; i < scanner.nextLine().length(); i++) {
+                Organism.name += scanner.nextLine().charAt(i);
+            }
+            if (Organism.name.equals("a")){
+                break;
+            }
+            LifeNest.organismlist.add(Organism.name);
+            LifeNest.management();
+        }
     }
 }
